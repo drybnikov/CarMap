c8297b214845e3dfbf52f05b48a89229aa67890e
drybnikau <drybnikau@justaddmusic.net>
2019-10-18 17:34:52 +0300

    Added car markers to map

--- app/src/main/java/com/test/denis/carmap/ui/CarsMapFragment.kt
+++ app/src/main/java/com/test/denis/carmap/ui/CarsMapFragment.kt
@@ -1,18 +1,52 @@
 package com.test.denis.carmap.ui
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.test.denis.carmap.R
+import com.test.denis.carmap.di.AbstractViewModelFactory
+import com.test.denis.carmap.di.Injectable
+import com.test.denis.carmap.model.CarModel
+import com.test.denis.carmap.network.Resource
+import com.test.denis.carmap.viewmodel.CarsViewModel
+import javax.inject.Inject
 
-class CarsMapFragment : SupportMapFragment(), OnMapReadyCallback {
+
+class CarsMapFragment : SupportMapFragment(), Injectable, OnMapReadyCallback {
+
+    @Inject
+    lateinit var factory: AbstractViewModelFactory<CarsViewModel>
 
     private lateinit var map: GoogleMap
+    private lateinit var viewModel: CarsViewModel
+
+    private val markerIconWidth by lazy {
+        resources.getDimensionPixelSize(R.dimen.marker_width)
+    }
 
     override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
 
         getMapAsync(this)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        activity?.let {
+            viewModel = ViewModelProviders.of(it, factory).get(CarsViewModel::class.java)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -24,5 +58,71 @@ class CarsMapFragment : SupportMapFragment(), OnMapReadyCallback {
             isTrafficEnabled = true
             uiSettings.isZoomControlsEnabled = true
         }
+
+        viewModel.loadCarsData().observe(this, Observer { onDataLoaded(it) })
+    }
+
+    private fun onDataLoaded(resource: Resource<List<CarModel>>) {
+        when (resource) {
+            is Resource.Success -> showCarsOnMap(resource.data)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun showCarsOnMap(carItems: List<CarModel>) {
+        val firstCar = carItems.first()
+        val carLatLng = LatLng(firstCar.latitude, firstCar.longitude)
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(carLatLng, DEFAULT_ZOOM))
+
+        addCarMarkers(carItems)
+    }
+
+    private fun addCarMarkers(carItems: List<CarModel>) {
+        val context = context ?: return
+        val glide = Glide.with(context)
+
+        carItems.forEach {
+            glide
+                .asBitmap()
+                .load(it.imageUrl)
+                .override(markerIconWidth, markerIconWidth)
+                .into(customGlideTarget(it))
+        }
+    }
+
+    private fun customGlideTarget(model: CarModel): CustomTarget<Bitmap> {
+        return object : CustomTarget<Bitmap>(markerIconWidth, markerIconWidth) {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                addMarker(resource, model)
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {
+                addMarker(null, model)
+            }
+
+            override fun onLoadFailed(errorDrawable: Drawable?) {
+                addMarker(null, model)
+            }
+        }
+    }
+
+    private fun addMarker(resource: Bitmap?, model: CarModel) {
+        val carLatLng = LatLng(model.latitude, model.longitude)
+
+        val markerOptions = MarkerOptions()
+            .position(carLatLng)
+            .title("${model.modelName} ${model.name}")
+            .snippet(model.licensePlate)
+
+        if (resource != null) {
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(resource)).flat(false)
+        }
+
+        map.addMarker(markerOptions)
+    }
+
+    companion object {
+        private const val DEFAULT_ZOOM = 11.0f
     }
 }
